cbf0bc6f441882ac6b041f3ef97a90593d1d7ad4
Leandro Martin <63266302+LeandroJMartin@users.noreply.github.com>
2022-12-21 17:39:16 -0300

    Update global.php

--- class/global.php
+++ class/global.php
@@ -9,17 +9,3 @@ error_reporting(E_ALL);
 define('DIR', 'https://'.$_SERVER['HTTP_HOST'].'/painel');
 define('DIRIMG', 'https://'.$_SERVER['HTTP_HOST'].'/assets/img');
 define('DIRPORT', 'https://'.$_SERVER['HTTP_HOST'].'/assets/portfolio');
-
-//require 'actions/action-files.php';
-
-/*
-echo $_SERVER['SERVER_PROTOCOL'].'<br>';
-echo $_SERVER['HTTPS'].'<br>';
-echo $_SERVER['HTTP_HOST'].'<br>';
-echo $_SERVER['SCRIPT_NAME'].'<br>';
-echo $_SERVER['QUERY_STRING'].'<br>';
-echo $_SERVER['REQUEST_URI'].'<br>';
-echo $_SERVER['SERVER_NAME'].'<br>';
-
-echo $_SERVER['REQUEST_METHOD'].'<br>';
-*/
